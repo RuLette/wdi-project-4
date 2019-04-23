2f3ba6e591bbca0d94ede53dc3eb16142dc627c5
Ru <firegalru@hotmail.com>
2019-04-23 19:36:41 +0100

    changed database name.

--- config/environment.py
+++ config/environment.py
@@ -1,4 +1,4 @@
 import os
 
-db_uri = os.getenv('DATABASE_URL', 'postgres://localhost:5432/qrafts')
+db_uri = os.getenv('DATABASE_URL', 'postgres://localhost:5432/qraft')
 secret = os.getenv('SECRET', 'it\s a secret')
